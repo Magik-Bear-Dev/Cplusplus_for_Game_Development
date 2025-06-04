e656a8948b0af3f1993d65a990f116f44c15decc
Magik Bear <magik.bear.dev@outlook.com>
2025-06-04 17:27:40 +0200

    Increment Operators

---
+++ Sample C++ Source Code 1/IncrementOperators.cpp
@@ -0,0 +1,43 @@
+#include "iostream" // header file
+using namespace std; // namespace
+
+int main() // main function
+{
+    int i = 3; // declaration and initialization
+    float j = 2.0; // declaration and initialization
+
+    int k = j/i; // division operator
+
+    cout << k << endl; // print the value of k
+    
+    system("pause"); // pause the system
+}
+
+/*
+================================================================
+This is extra code to demonstrate the increment operator examples
+
+Code example 1.
+
+int i = 3; // declaration and initialization
+
+cout << --i << endl; // decrement operator
+cout << i << endl; // print the value of i
+
+Code example 2.
+
+int i = 3; // declaration and initialization
+
+--i; // decrement operator
+
+cout << i << endl; // print the value of i
+
+Code example 3.
+
+int i - 3; // declaration and initialization
+
+i--; // decrement operator
+
+cout << i << endl; // print the value of i
+================================================================
+ */
