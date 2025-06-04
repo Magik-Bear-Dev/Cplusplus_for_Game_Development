7e228ddda24e779bee1b56180cd914e264b7d5ff
Magik Bear <magik.bear.dev@outlook.com>
2025-06-04 17:33:46 +0200

    Objects And Classes

---
+++ Sample C++ Source Code 1/ObjectsAndClasses.cpp
@@ -0,0 +1,24 @@
+#include "iostream" // header file
+#include "string" // header file
+using namespace std; // namespace
+
+class Dog // class
+{
+    string Name; // declaration
+    int Age; // declaration
+    float Health; // declaration
+
+    void Bark() // function
+    {
+        cout << "Woof! Woof!" << endl; // print the message
+    }
+};
+
+int main() // main function
+{
+    Dog Spot; // declaration and initialization
+    Dog Rex; // declaration and initialization
+    
+    
+    system("pause"); // pause the system
+}
